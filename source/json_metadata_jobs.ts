23f3b8ab7821066b8b116ed52e13ed1db3ef7931
Joel Ek <contact@joelek.se>
2022-04-11 15:30:58 +0200

    Produce show metadata files.

--- source/json_metadata_jobs.ts
+++ source/json_metadata_jobs.ts
@@ -3,12 +3,12 @@ import * as discdb from "./discdb";
 import * as job from "./job";
 import * as utils from "./utils";
 
-async function getMetadata(database: discdb.MediaDatabase, basename: string): Promise<{
+function getMetadata(database: discdb.MediaDatabase, basename: string): {
 	id: string,
 	index: number,
 	media: discdb.Media,
 	track: discdb.MediaContent
-}> {
+} {
 	const parts = basename.split(".");
 	if (parts.length === 3) {
 		const id = parts[0];
@@ -29,43 +29,63 @@ async function getMetadata(database: discdb.MediaDatabase, basename: string): Pr
 	throw "Unable to get metadata!";
 }
 
-async function getTargetPaths(media: discdb.Media, track: discdb.MediaContent): Promise<Array<string>> {
-	if (discdb.EpisodeContent.is(track)) {
-		let show = utils.pathify(track.show);
-		let season_number = ("00" + track.season).slice(-2);
-		let episode_number = ("00" + track.episode).slice(-2);
-		let title = utils.pathify(track.title);
-		let suffix = utils.pathify(media.type);
-		return [
-			".",
-			"private",
-			"media",
-			"video",
-			"shows",
-			`${show}`,
-			`s${season_number}`,
-			`${show}-s${season_number}e${episode_number}-${title}-${suffix}`,
-			`00-metadata`
-		];
-	}
-	if (discdb.MovieContent.is(track)) {
-		let title = utils.pathify(track.title);
-		let year = ("0000" + track.year).slice(-4);
-		let suffix = utils.pathify(media.type);
-		let dir = title.substr(0, 1);
-		return [
-			".",
-			"private",
-			"media",
-			"video",
-			"movies",
-			`${dir}`,
-			`${title}-${year}-${suffix}`,
-			`00-metadata`,
-		];
-	}
-	throw "Unable to get path!";
+function getShowTargetPath(media: discdb.Media, track: discdb.EpisodeContent): Array<string> {
+	let show = utils.pathify(track.show);
+	return [
+		".",
+		"private",
+		"media",
+		"video",
+		"shows",
+		`${show}`,
+		`00-metadata`
+	];
 }
+
+function getEpisodeTargetPath(media: discdb.Media, track: discdb.EpisodeContent): Array<string> {
+	let show = utils.pathify(track.show);
+	let season_number = ("00" + track.season).slice(-2);
+	let episode_number = ("00" + track.episode).slice(-2);
+	let title = utils.pathify(track.title);
+	let suffix = utils.pathify(media.type);
+	return [
+		".",
+		"private",
+		"media",
+		"video",
+		"shows",
+		`${show}`,
+		`s${season_number}`,
+		`${show}-s${season_number}e${episode_number}-${title}-${suffix}`,
+		`00-metadata`
+	];
+}
+
+function getMovieTargetPath(media: discdb.Media, track: discdb.MovieContent): Array<string> {
+	let title = utils.pathify(track.title);
+	let year = ("0000" + track.year).slice(-4);
+	let suffix = utils.pathify(media.type);
+	let dir = title.substr(0, 1);
+	return [
+		".",
+		"private",
+		"media",
+		"video",
+		"movies",
+		`${dir}`,
+		`${title}-${year}-${suffix}`,
+		`00-metadata`,
+	];
+}
+
+type ShowMetadata = {
+	type: "show",
+	imdb: string,
+	title: string,
+	summary: string,
+	genres: string[],
+	actors: string[]
+};
 
 type EpisodeMetadata = {
 	type: "episode",
@@ -94,39 +114,46 @@ type MovieMetadata = {
 	actors: string[]
 };
 
-async function getJson(media: discdb.Media, track: discdb.MediaContent): Promise<EpisodeMetadata | MovieMetadata> {
-	if (discdb.EpisodeContent.is(track)) {
-		const json: EpisodeMetadata = {
-			type: "episode",
-			imdb: track.imdb,
-			title: track.title,
-			year: track.year,
-			summary: track.summary,
-			show: {
-				imdb: track.imdb_show,
-				title: track.show,
-				summary: track.summary_show,
-				genres: track.genres_show,
-				actors: track.actors_show
-			},
-			season: track.season,
-			episode: track.episode
-		};
-		return json;
-	}
-	if (discdb.MovieContent.is(track)) {
-		const json: MovieMetadata = {
-			type: "movie",
-			imdb: track.imdb,
-			title: track.title,
-			year: track.year,
-			summary: track.summary,
-			genres: track.genres,
-			actors: track.actors
-		};
-		return json;
-	}
-	throw "Unable to get json!";
+function getShowJson(media: discdb.Media, track: discdb.EpisodeContent): ShowMetadata {
+	return {
+		type: "show",
+		imdb: track.imdb_show,
+		title: track.show,
+		summary: track.summary_show,
+		genres: track.genres_show,
+		actors: track.actors_show
+	};
+}
+
+function getEpisodeJson(media: discdb.Media, track: discdb.EpisodeContent): EpisodeMetadata {
+	return {
+		type: "episode",
+		imdb: track.imdb,
+		title: track.title,
+		year: track.year,
+		summary: track.summary,
+		show: {
+			imdb: track.imdb_show,
+			title: track.show,
+			summary: track.summary_show,
+			genres: track.genres_show,
+			actors: track.actors_show
+		},
+		season: track.season,
+		episode: track.episode
+	};
+}
+
+function getMovieJson(media: discdb.Media, track: discdb.MovieContent): MovieMetadata {
+	return {
+		type: "movie",
+		imdb: track.imdb,
+		title: track.title,
+		year: track.year,
+		summary: track.summary,
+		genres: track.genres,
+		actors: track.actors
+	};
 }
 
 async function createJobListRecursively(database: discdb.MediaDatabase, directories: Array<string>): Promise<Array<job.PromiseJob>> {
@@ -144,22 +171,48 @@ async function createJobListRecursively(database: discdb.MediaDatabase, director
 			continue;
 		}
 		if (entry.isFile() && basename.endsWith(".mkv")) {
-			async function perform(): Promise<void> {
-				const metadata = await getMetadata(database, basename);
-				const media = metadata.media;
-				const track = metadata.track;
-				const paths = await getTargetPaths(media, track);
-				const path = paths.join("/") + ".json";
-				if (!libfs.existsSync(path)) {
-					console.log(path);
-					libfs.mkdirSync(paths.slice(0, -1).join("/"), { recursive: true });
-					const json = await getJson(media, track);
-					libfs.writeFileSync(path, JSON.stringify(json, null, "\t") + "\n");
-				}
+			const metadata = getMetadata(database, basename);
+			const media = metadata.media;
+			const track = metadata.track;
+			if (discdb.EpisodeContent.is(track)) {
+				jobs.push({
+					perform: async () => {
+						const paths = getShowTargetPath(media, track);
+						const path = paths.join("/") + ".json";
+						if (!libfs.existsSync(path)) {
+							console.log(path);
+							libfs.mkdirSync(paths.slice(0, -1).join("/"), { recursive: true });
+							const json = getShowJson(media, track);
+							libfs.writeFileSync(path, JSON.stringify(json, null, "\t") + "\n");
+						}
+					}
+				});
+				jobs.push({
+					perform: async () => {
+						const paths = getEpisodeTargetPath(media, track);
+						const path = paths.join("/") + ".json";
+						if (!libfs.existsSync(path)) {
+							console.log(path);
+							libfs.mkdirSync(paths.slice(0, -1).join("/"), { recursive: true });
+							const json = getEpisodeJson(media, track);
+							libfs.writeFileSync(path, JSON.stringify(json, null, "\t") + "\n");
+						}
+					}
+				});
+			} else if (discdb.MovieContent.is(track)) {
+				jobs.push({
+					perform: async () => {
+						const paths = getMovieTargetPath(media, track);
+						const path = paths.join("/") + ".json";
+						if (!libfs.existsSync(path)) {
+							console.log(path);
+							libfs.mkdirSync(paths.slice(0, -1).join("/"), { recursive: true });
+							const json = getMovieJson(media, track);
+							libfs.writeFileSync(path, JSON.stringify(json, null, "\t") + "\n");
+						}
+					}
+				});
 			}
-			jobs.push({
-				perform
-			});
 			continue;
 		}
 	}
