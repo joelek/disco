83a9c4df1b18d8b735345f67ac20e5b97ecaf795
Joel Ek <contact@joelek.se>
2023-06-30 11:59:37 +0200

    Output album disc metadata files.

--- source/cover_art_transcode_jobs.ts
+++ source/cover_art_transcode_jobs.ts
@@ -34,10 +34,9 @@ async function getPaths(media: cddb.Disc): Promise<Array<string>> {
 		"media",
 		"audio",
 		`${disc_artist}`,
-		`${disc_artist}-${disc_year}-${disc_title}-${disc_number}-${suffix}`,
-		`00-artwork`,
+		`${disc_artist}-${disc_year}-${disc_title}-${disc_number}-${suffix}`
 	];
-}
+};
 
 async function writeBufferToDisk(buffer: Buffer, path: string): Promise<void> {
 	return new Promise((resolve, reject) => {
@@ -61,6 +60,38 @@ async function writeBufferToDisk(buffer: Buffer, path: string): Promise<void> {
 	});
 }
 
+type AlbumMetadata = {
+	type: "album";
+	title: string;
+	disc: number;
+	year?: number;
+	artists: string[];
+	tracks: {
+		title: string;
+		artists: string[];
+		copyright?: string;
+	}[];
+	copyright?: string;
+	tidal?: number;
+};
+
+function getAlbumMetadata(disc: cddb.Disc): AlbumMetadata {
+	return {
+		type: "album",
+		title: disc.title,
+		disc: disc.number,
+		year: disc.year,
+		artists: disc.artists,
+		tracks: disc.tracks.map((track) => ({
+			title: track.title,
+			artists: track.artists,
+			copyright: undefined
+		})),
+		copyright: undefined,
+		tidal: disc.tidal
+	};
+};
+
 async function createJobListRecursively(database: cddb.Database, directories: Array<string>): Promise<Array<job.PromiseJob>> {
 	const jobs = new Array<job.PromiseJob>();
 	const entries = await libfs.promises.readdir(directories.join("/"), {
@@ -76,22 +107,34 @@ async function createJobListRecursively(database: cddb.Database, directories: Ar
 			continue;
 		}
 		if (entry.isFile() && basename.endsWith(".jpg")) {
-			async function perform(): Promise<void> {
-				const metadata = await getMetadata(database, basename);
-				const paths = await getPaths(metadata.media);
-				const path = paths.join("/") + ".jpg";
-				if (!libfs.existsSync(path)) {
-					console.log(path);
-					const buffer = libfs.readFileSync([
-						...directories,
-						basename
-					].join("/"));
-					libfs.mkdirSync(paths.slice(0, -1).join("/"), { recursive: true });
-					await writeBufferToDisk(buffer, path);
-				}
-			}
 			jobs.push({
-				perform
+				async perform() {
+					const metadata = await getMetadata(database, basename);
+					const paths = await getPaths(metadata.media);
+					const path = paths.join("/") + "00-artwork.jpg";
+					if (!libfs.existsSync(path)) {
+						console.log(path);
+						const buffer = libfs.readFileSync([
+							...directories,
+							basename
+						].join("/"));
+						libfs.mkdirSync(paths.slice(0, -1).join("/"), { recursive: true });
+						await writeBufferToDisk(buffer, path);
+					}
+				}
+			});
+			jobs.push({
+				async perform() {
+					const metadata = await getMetadata(database, basename);
+					const paths = await getPaths(metadata.media);
+					const path = paths.join("/") + "00-metadata.json";
+					if (!libfs.existsSync(path)) {
+						console.log(path);
+						libfs.mkdirSync(paths.slice(0, -1).join("/"), { recursive: true });
+						const json = getAlbumMetadata(metadata.media);
+						libfs.writeFileSync(path, JSON.stringify(json, null, "\t") + "\n");
+					}
+				}
 			});
 			continue;
 		}
